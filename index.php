3decc93ae7b7ab0b90b8258cccee7092661f9141
IlyanaIlyana <73875070+IlyanaIlyana@users.noreply.github.com>
2021-12-08 20:32:44 +0300

    переносила функции в art_control art_view

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ require_once "functions_control_view_login_on_index.php"; //= functions_for_page
 require_once "function_view_on_index.php"; // = function_for_page_view_on_index.php
 require_once "header.php"; 	
 /* require_once "functions_preparing_data_to_show.php";  */
-require_once "functions_selecting_data.php"; 
-require_once "functions_showing_data.php"; 
+require_once "art_control/functions_selecting_data.php"; 
+require_once "art_view/functions_showing_data.php"; 
 	
 	StartDB();						
 	CheckLogin(); //in functions_for_page_view_login_on_index.php - function_view_on_index.php
