65cae52a7503133481ecad13084e64f28f97ceeb
IlyanaIlyana <73875070+IlyanaIlyana@users.noreply.github.com>
2021-12-09 21:45:43 +0300

    перенесла в подпапку art_control

--- art_control/processing_add_task_to_case.php
+++ art_control/processing_add_task_to_case.php
@@ -5,14 +5,16 @@ require_once "start_mysql.php";
 	
 	//print_r($_POST);     
 	$id_empl = $_SESSION['userid'];
-	$case_id = $_POST['case_id'];
+	$case_id = $_POST['caseid'];
     $task_id = $_POST['taskid'];   
     $responsible_id = $_POST['responsible_id']; 
     $start_term = $_POST['start_term']; 
 	$end_term = $_POST['end_term'];   
+
 	if (empty($_POST['responsible_id'])){$responsible_id = $id_empl;}
-	/* if (empty($_POST['start_term'])){$start_term = date("y-m-d");}
-	if (empty($_POST['end_term'])){$end_term = date("y-m-d");} */
+
+	if (empty($_POST['start_term'])){$start_term = date("y-m-d");}
+	if (empty($_POST['end_term'])){$end_term = date("y-m-d");}
 
 	if (!empty($_POST['what_id'])){$what_id = $_POST['what_id'];}
     else {$what_id = 1;}
@@ -31,8 +33,8 @@ require_once "start_mysql.php";
 			printf("Ошибка добавления задачи: %s\n", mysqli_error($db));
 		}
 		EndDB();
-	header("Refresh: 1; URL = page_for_case_card.php?sent_case_id=$case_id");
-	
+/* 	header("Refresh: 1; URL = ../page_for_case_card.php?sent_case_id=$case_id"); */
+	header("Refresh: 1; URL = ../index.php");	
 	
 ?>
 
